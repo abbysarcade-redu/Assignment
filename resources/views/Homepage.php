922fd48923018563b8ea4f04a7042f88d2a84271
Abby <a.donkin@redu.co.uk>
2021-04-26 23:50:54 +0100

    updated nav

--- resources/views/Homepage.php
+++ resources/views/Homepage.php
@@ -14,44 +14,46 @@
 </head>
 <body>
 <!---NAVBAR --->
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#"><img src="Style/images/Untitled design.png" alt="Game On" width="80"></a>
-
-    <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-            <li class="nav-item active">
-                <a class="nav-link " href="about.php">About</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="products.php">Products</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="contact%20page.php">Contact</a>
-            </li>
-            <li>
-                <input class="search-form" type="text" placeholder="Search..">
-            </li>
-            <li class="nav-item img">
-                <a class="img" href="basket.php">
-                    <img id="accountImage" srC="Style/images/Basket.png" alt="Basket" width="40" height="40">
-                </a>
-            </li>
-            <li class="nav-item img">
-                <a class="img" href="#">
-                    <img id="accountImage" src="Style/images/Account.png" alt="Account" width="40" height="40">
-                </a>
-            </li>
-        </ul>
-    </div>
+<nav class="header">
+    <a href="" class="logo"><img src="Style/images/Untitled design.png" alt="Game On" width="80"></a>
+    <input class="menu-btn" type="checkbox" id="menu-btn" />
+    <label class="menu-icon" for="menu-btn"><span class="navicon"></span></label>
+    <ul class="menu">
+        <li class="nav-item active">
+            <a class="nav-link " href="about.php">About</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="products.php">Products</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="contact%20page.php">Contact</a>
+        </li>
+        <li>
+            <input class="search-form" type="text" placeholder="Search..">
+        </li>
+        <li class="img">
+            <a class="img" href="basket.php">
+                <img id="accountImage" srC="Style/images/Basket.png" alt="Basket" width="40" height="40">
+            </a>
+        </li>
+        <li class="img">
+            <a class="img" href="#">
+                <img id="accountImage" src="Style/images/Account.png" alt="Account" width="40" height="40">
+            </a>
+        </li>
+    </ul>
 </nav>
+<div class="nav-buffer">
+
+</div>
 <div class="home-page">
     <div class="hero-image">
         <img src="Style/images/Homepage-banner.png" class="hero">
     </div>
     <div class="pop-console">
         <h1>Popular Consoles</h1>
-        <div>
-            <img src="Style/images/ps-logo.png" class="pop-console-img" href="#">
+        <div class="pop-console-container">
+            <img src="Style/images/ps-logo.png" class="pop-console-img" href="about.php">
             <img src="Style/images/Xbox-logo.png" class="pop-console-img">
             <img src="Style/images/nintendo-logo.png" class="pop-console-img">
             <img src="Style/images/computer.png" class="pop-console-img">
---
+++ resources/views/playstation.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UFT-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=VT323&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Space+Mono&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/app.css">
+    <title>Game On</title>
+</head>
+<body>
+<nav class="header">
+    <a href="" class="logo"><img src="Style/images/Untitled design.png" alt="Game On" width="80"></a>
+    <input class="menu-btn" type="checkbox" id="menu-btn" />
+    <label class="menu-icon" for="menu-btn"><span class="navicon"></span></label>
+    <ul class="menu">
+        <li class="nav-item active">
+            <a class="nav-link " href="about.php">About</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="products.php">Products</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="contact%20page.php">Contact</a>
+        </li>
+        <li>
+            <input class="search-form" type="text" placeholder="Search..">
+        </li>
+        <li class="img">
+            <a class="img" href="basket.php">
+                <img id="accountImage" srC="Style/images/Basket.png" alt="Basket" width="40" height="40">
+            </a>
+        </li>
+        <li class="img">
+            <a class="img" href="#">
+                <img id="accountImage" src="Style/images/Account.png" alt="Account" width="40" height="40">
+            </a>
+        </li>
+    </ul>
+</nav>
+<div><h1>hello</h1></div>
