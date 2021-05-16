0b41b6d7d0d53b127ff0286ad1aa48ba1551f525
Abby <a.donkin@redu.co.uk>
2021-05-16 02:32:54 +0100

    Reduce Useful links nav buffer

--- resources/views/useful_links.php
+++ resources/views/useful_links.php
@@ -5,7 +5,6 @@
 </header>
 <body>
 <?php include ("layout/nav.php"); ?>
-<div class="nav-buffer"></div>
 <div class="content-page">
     <div class="hero-image">
         <img src="Style/images/useful-links-banner.png" class="hero">
