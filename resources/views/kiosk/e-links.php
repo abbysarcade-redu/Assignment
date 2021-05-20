eb891be3e06ba60b969c74b355e5de014d172a17
Abby <a.donkin@redu.co.uk>
2021-05-20 22:02:51 +0100

    edit link dropdown function

--- resources/views/kiosk/e-links.php
+++ resources/views/kiosk/e-links.php
@@ -37,12 +37,37 @@ $link = $kiosk_link_preparedStatement->fetch();?>
             <h1 class="kiosk_title">Language</h1>
             <?php $lang_preparedStatement->execute([$link['lang']]);
             $link_lang = $lang_preparedStatement->fetch(); ?>
-            <input name="lang" class="kiosk_input" value="<?php echo($link_lang['language_name']) ?>"/>
+            <select class="kiosk_drop" name="lang">
+                <?php $get_lang_preparedStatement->execute();
+                $row = $get_lang_preparedStatement->fetchAll();
+                print_r($row);
+                foreach ($row as $lang=>$name):
+                    if ($link_lang['language_name'] == $name['language_name']):?>
+                        <option value="<?php print_r($name['language_id']) ?>" selected>
+                            <?php print_r($name['language_name']) ?>
+                        </option>
+                    <?php else: ?>
+                        <option value="<?php print_r($name['language_id']) ?>">
+                            <?php print_r($name['language_name']) ?>
+                        </option>
+                    <?php endif;?>
+                <?php endforeach; ?>
+            </select>
             <hr>
             <h1 class="kiosk_title">Tags</h1>
             <?php $tag_preparedStatement->execute([$link['tags']]);
             $link_tag = $tag_preparedStatement->fetch();?>
-            <input name="tags" class="kiosk_input" value="<?php echo($link_tag['tag_name']) ?>"/>
+            <select class="kiosk_drop" name="tags">
+                <?php $get_tag_preparedStatement->execute();
+                $row = $get_tag_preparedStatement->fetchAll();
+                foreach ($row as $tag=>$name):
+                    if ($link_tag['tag_name'] == $name['tag_name']):?>
+                        <option value="<?php print_r($name['tag_id']) ?>" selected><?php print_r($name['tag_name']) ?></option>
+                    <?php else: ?>
+                        <option value="<?php print_r($name['tag_id']) ?>"><?php print_r($name['tag_name']) ?></option>
+                    <?php endif; ?>
+                <?php endforeach; ?>
+            </select>
             <hr>
             <h1 class="kiosk_title">URL</h1>
             <input name="url" class="kiosk_input" value="<?php echo($link['url']) ?>"/>
